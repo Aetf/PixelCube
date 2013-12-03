b365cb79331a77abbf678afa9ec99578b030213f
Silentの尛明 <wym981230@126.com>
2013-12-03 22:46:50 +0800

    和xp准备进行分支融合

--- src/PixelCube/LeapMotion/LeapListener.cs
+++ src/PixelCube/LeapMotion/LeapListener.cs
@@ -22,21 +22,30 @@ namespace PixelCube.LeapMotion
         /// <summary>
         /// All events this class will offer
         /// </summary>
-        public event EventHandler<DeviceInfoArgs> LeapStatusInfom;
-        public event EventHandler<Vector> Dye;
-        public event EventHandler<Vector> Move;
-        public event EventHandler<EventArgs> Rotate;
-        public event EventHandler<EventArgs> Zoom;
+        public event EventHandler<DeviceInfoArgs> LeapStatusEvent;
+        public event EventHandler<Vector> PreDrawOperationEvent;
+        public event EventHandler<Vector> PreFocusOperationEvent;
+        public event EventHandler<EventArgs> PreRotateOperationEvent;
+        public event EventHandler<float> PreScaleOperationEvent;
 
     /* Attributes */
         private int pointableID;
-        private bool isDyeing;
+        private bool isDrawing;
+        private Frame lastFrame;
+        private Frame currentFrame;
         
         
+        /// <summary>
+        ///     Initialize the private attributes
+        /// </summary>
+        /// <param name="controller"> Leap Controller </param>
         public override void OnInit(Controller controller)
         {
             pointableID = -1;
-            isDyeing = false;
+            isDrawing = false;
+            lastFrame = null;
+            currentFrame = null;
+            
             base.OnInit(controller);
         }
 
@@ -48,17 +57,17 @@ namespace PixelCube.LeapMotion
         /// </param>
         public override void OnConnect(Controller controller)
         {
-            EventHandler<DeviceInfoArgs> leapStatusInfom = LeapStatusInfom;
+            EventHandler<DeviceInfoArgs> leapStatusEvent = LeapStatusEvent;
             //controller.EnableGesture(Gesture.GestureType.TYPECIRCLE);
             controller.EnableGesture(Gesture.GestureType.TYPEKEYTAP);
             controller.EnableGesture(Gesture.GestureType.TYPESCREENTAP);
-            controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
+            //controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
 
-            if (leapStatusInfom != null)
+            if (leapStatusEvent != null)
             {
                 DeviceInfoArgs deviceInfoArg = new DeviceInfoArgs();
                 deviceInfoArg.isConnected = true;
-                leapStatusInfom(this, deviceInfoArg);
+                leapStatusEvent(this, deviceInfoArg);
             }
 
             base.OnConnect(controller);
@@ -71,12 +80,12 @@ namespace PixelCube.LeapMotion
         /// <param name="controller"> Leap Controller </param>
         public override void OnDisconnect(Controller controller)
         {
-            EventHandler<DeviceInfoArgs> leapStatusInfom = LeapStatusInfom;
-            if (leapStatusInfom != null)
+            EventHandler<DeviceInfoArgs> leapStatusEvent = LeapStatusEvent;
+            if (leapStatusEvent != null)
             {
                 DeviceInfoArgs deviceInfoArg = new DeviceInfoArgs();
                 deviceInfoArg.isConnected = false;
-                leapStatusInfom(this, deviceInfoArg);
+                leapStatusEvent(this, deviceInfoArg);
             }
             base.OnDisconnect(controller);
         }
@@ -96,54 +105,74 @@ namespace PixelCube.LeapMotion
         /// <param name="controller"> Controller this listener belongs to</param>
         public override void OnFrame(Controller controller)
         {
-            EventHandler<Vector> move = Move;
-            Frame frame = controller.Frame();
-            PointableList pointableList = frame.Pointables;
-            if (!pointableList.IsEmpty)
+            currentFrame = controller.Frame();
+
+            // If has two hands, suppose it will has a scale operation soon.
+            if (currentFrame.Hands.Count > 2)
             {
-                Pointable pointable = frame.Pointable(pointableID);
-                if (!pointable.IsValid) // Pointable is invalid, track a new one
-                {
-                    pointable = frame.Pointables[0];
-                    pointableID = pointable.Id;
-                }
-
-                GestureList gestureList = frame.Gestures();
-                foreach (Gesture gesture in gestureList)
-                {
-                    switch (gesture.Type)
-                    {
-                        case Gesture.GestureType.TYPESCREENTAP:
-                            //ScreenTapGesture screenTapGesture = (ScreenTapGesture)gesture;
-                            if (Dye != null)
-                            {
-                                EventHandler<Vector> dye = Dye;
-                                dye(this, ((ScreenTapGesture)gesture).Position);
-                            }
-                            break;
-                        case Gesture.GestureType.TYPEKEYTAP:
-                            // Enter or exit dying mode
-                           isDyeing = isDyeing ? false : true;
-                           break;
-
-
-
-
-
-
-
-
-                    }
-                }
-
-
-                // move to a new position
-                if (move != null)
-                {
-                    move(this, pointable.TipPosition);
-                }
+                EventHandler<float> scale = PreScaleOperationEvent;
+                scale(this, currentFrame.ScaleFactor(lastFrame));
                 
             }
+
+            if (currentFrame.Hands.Count == 1)
+            {
+                Hand hand = currentFrame.Hands[0];
+
+
+            }
+
+
+
+
+
+
+
+            //PointableList pointableList = frame.Pointables;
+            //if (!pointableList.IsEmpty)
+            //{
+            //    Pointable pointable = frame.Pointable(pointableID);
+            //    if (!pointable.IsValid) // Pointable is invalid, track a new one
+            //    {
+            //        pointable = frame.Pointables[0];
+            //        pointableID = pointable.Id;
+            //    }
+
+            //    GestureList gestureList = frame.Gestures();
+            //    foreach (Gesture gesture in gestureList)
+            //    {
+            //        switch (gesture.Type)
+            //        {
+            //            case Gesture.GestureType.TYPESCREENTAP:
+            //                //ScreenTapGesture screenTapGesture = (ScreenTapGesture)gesture;
+            //                if (PreDrawOperationEvent != null)
+            //                {
+            //                    EventHandler<Vector> draw = PreDrawOperationEvent;
+            //                    draw(this, ((ScreenTapGesture)gesture).Position);
+            //                }
+            //                break;
+
+            //            case Gesture.GestureType.TYPEKEYTAP:
+            //                // Enter or exit dying mode
+            //               isDrawing = isDrawing ? false : true;
+            //               break;
+
+            //           // case Gesture.GestureType.TYPESWIPE:
+
+            //           //    break;
+
+
+            //        }
+            //    }
+
+
+            //    // move to a new position
+            //    if (focus != null)
+            //    {
+            //        focus(this, pointable.TipPosition);
+            //    }
+                
+            //}
             base.OnFrame(controller);
         }
 
