9f1a3900bef9a5e06a7c4e85b67d9e3116c5e030
Aetf <7437103@gmail.com>
2013-12-04 00:52:53 +0800

    两个接口的访问权限设置改成public

--- src/PixelCube/Interfaces/IVolume.cs
+++ src/PixelCube/Interfaces/IVolume.cs
@@ -9,7 +9,7 @@ namespace PixelCube.Interfaces
     /// <summary>
     /// 实现此接口以具有声音调整相关的功能
     /// </summary>
-    interface IVolume
+    public interface IVolume
     {
         /// <summary>
         /// 获取和设置是否静音
