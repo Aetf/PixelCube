5af61e3be35bf3bed627ad7fa6437e367d238d8c
右耳树灵 <420449453@qq.com>
2013-12-05 00:17:38 +0800

    Add the class of BackgroundMusic and BackgroundSound

---
+++ src/PixelCube/Sound/BackgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace PixelCube.Sound
+{
+    /// <summary>
+    /// 背景音乐循环播放
+    /// </summary>
+    public class BackgroundMusic
+    {
+        public bool isOpen;
+        public void PlayMusic(bool open)
+        {
+            System.Media.SoundPlayer Mu = new System.Media.SoundPlayer("f:\\song.wav");
+            open = isOpen;
+            if (open == true)
+                Mu.PlayLooping();
+            else
+                Mu.Stop();
+        }
+    }
+}
---
+++ src/PixelCube/Sound/BackgroundSound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace PixelCube.Sound
+{
+    public class BackgroundSound
+    {
+        public bool StatusChanged;
+        public bool GetStatus
+        {
+            get;
+            set;
+
+
+        }
+        public void PlaySound(bool play)
+        {
+            play = StatusChanged;
+            SoundPlayer Sound = new SoundPlayer("");
+            if (play == true)
+                Sound.Play();
+            else
+                Sound.Dispose();
+        }
+    }
+}
