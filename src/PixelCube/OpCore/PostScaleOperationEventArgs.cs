4de8fca4a0d59cc8b029f20ff7a1fc7290fec6d9
duranl <dongyangluo@gmail.com>
2013-12-04 09:44:16 +0800

    核心操作类框架完善（添加了LeapMotion事件响应空函数），与LeapMotion采用相似的目录结构（将事件参数类独立成单独文件）

---
+++ src/PixelCube/OpCore/PostScaleOperationEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelCube.OpCore
+{
+    /// <summary>
+    /// 缩放事件参数
+    /// </summary>
+    public class PostScaleOperationEventArgs : EventArgs
+    {
+        public PostScaleOperationEventArgs()
+        {
+        }
+    }
+}
