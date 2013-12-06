1cc03aaca6f87cc28c45f57f108db1d8ee93c293
champion1zjp <380741208@qq.com>
2013-12-06 20:33:12 +0800

    完善了坐标转换的实现函数

--- src/PixelCube/LeapMotion/CoordinatesTrans.cs
+++ src/PixelCube/LeapMotion/CoordinatesTrans.cs
@@ -8,7 +8,39 @@ namespace PixelCube.LeapMotion
 {
     class CoordinatesTrans
     {
+        /// <summary>
+        /// 转换后的新坐标
+        /// </summary>
         private Vector newVec;
+
+        /// <summary>
+        /// 小方块边长
+        /// </summary>
+        private double cubea;
+
+        /// <summary>
+        /// 每行小方块数目
+        /// </summary>
+        private int cuben;
+
+        /// <summary>
+        /// 世界坐标的最大范围
+        /// </summary>
+        private float maxCoord;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public CoordinatesTrans()
+        {
+            newVec = new Vector(0, 0, 0);
+            ConfigProvider cp = ConfigProvider.Instance;
+            cubea = cp.CubeA;       //获取小方块边长
+
+            //在此处获取当前每行小方块数目
+
+            maxCoord =(float) cubea * cuben + 40;
+        }
 
         /// <summary>
         /// 获取转换后的坐标
@@ -19,16 +51,6 @@ namespace PixelCube.LeapMotion
             return newVec;
         }
 
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public CoordinatesTrans()
-        {
-            newVec = new Vector(0,0,0);
-            ConfigProvider cp = ConfigProvider.Instance;
-            //double cubea = cp.CubeA;
-
-        }
         /// <summary>
         /// 将LeapMotion坐标转换为世界坐标
         /// </summary>
@@ -42,9 +64,9 @@ namespace PixelCube.LeapMotion
             if (vec.x >= -300 && vec.x <= 300 && vec.y >= 30 && vec.y <= 630 && vec.z >= -300 && vec.z <= 300)
             {
                 //operations:
-                //newVec.x = (vec.x + 300) / () - 100;
-                //newVec.y = (vec.y - 30 ) / () - 100;
-                //newVec.x = (vec.z + 300) / () - 100;
+                newVec.x = (vec.x + 300) / (600 / maxCoord) - maxCoord/2 - 20;
+                newVec.y = (vec.y - 30) / (600 / maxCoord) - 20;
+                newVec.x = (vec.z + 300) / (600 / maxCoord) - maxCoord/2 - 20;
                 return true;
             }
             return false;
