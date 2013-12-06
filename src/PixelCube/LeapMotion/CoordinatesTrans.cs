2b12c9288d9a6399d892749b8e40de3821900ed4
champion1zjp <380741208@qq.com>
2013-12-06 20:05:03 +0800

    编写了坐标转换的实现方法

--- src/PixelCube/LeapMotion/CoordinatesTrans.cs
+++ src/PixelCube/LeapMotion/CoordinatesTrans.cs
@@ -8,12 +8,26 @@ namespace PixelCube.LeapMotion
 {
     class CoordinatesTrans
     {
-        
+        private Vector newVec;
+
+        /// <summary>
+        /// 获取转换后的坐标
+        /// </summary>
+        /// <returns>返回转换后得到的世界坐标</returns>
+        public Vector getNewVec()
+        {
+            return newVec;
+        }
+
         /// <summary>
         /// 构造函数
         /// </summary>
         public CoordinatesTrans()
         {
+            newVec = new Vector(0,0,0);
+            ConfigProvider cp = ConfigProvider.Instance;
+            //double cubea = cp.CubeA;
+
         }
         /// <summary>
         /// 将LeapMotion坐标转换为世界坐标
@@ -23,10 +37,17 @@ namespace PixelCube.LeapMotion
         /// <param name="maxY">世界坐标Y轴最大值</param>
         /// <param name="maxZ">世界坐标Z轴最大值</param>
         /// <returns>变换后的坐标</returns>
-        public void Trans(Vector vec, int maxX, int maxY, int maxZ)
+        public bool Trans(Vector vec)
         {
-
+            if (vec.x >= -300 && vec.x <= 300 && vec.y >= 30 && vec.y <= 630 && vec.z >= -300 && vec.z <= 300)
+            {
+                //operations:
+                //newVec.x = (vec.x + 300) / () - 100;
+                //newVec.y = (vec.y - 30 ) / () - 100;
+                //newVec.x = (vec.z + 300) / () - 100;
+                return true;
+            }
+            return false;
         }
-
     }
 }
